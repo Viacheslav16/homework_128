8b59d8238c7e174e3c33f7dd18dfd216aa947bc5
Viacheslav16 <slavalanchak16@gmail.com>
2024-01-25 20:56:57 +0100

    edited lesson 05 object

--- 05-js-object/assets/JS/dist/main.dev.js
+++ 05-js-object/assets/JS/dist/main.dev.js
@@ -6,84 +6,84 @@
 //3)Перевірка водія на наявність його ім’я у списку
 //4)Підрахунок необхідного часу та кількості палива для подолання переданої відстані з середньою швидкістю. Враховуй, що через кожні 4 години дороги водієві необхідно робити перерву на 1 годину. 
 
-function My_task1() {
-  var car = {
-    Brand: 'Skoda',
-    Model: 'Oktavia',
-    'Year of productin': 2009,
-    Color: 'DarkGrey',
-    'Max speed': 320,
-    Using: 6.5,
-    IsNew: false,
-    IsMechanic: true,
-    FuelCapacity: 55,
-    IndentNumber: 'XX0000YY',
-    DriverName: 'Viacheslav',
-    setDriverName: function setDriverName() {
-      var name = document.getElementById("name_one").value;
+var car = {
+  Brand: 'Skoda',
+  Model: 'Oktavia',
+  'Year of productin': 2009,
+  Color: 'DarkGrey',
+  'Max speed': 320,
+  Using: 6.5,
+  IsNew: false,
+  IsMechanic: true,
+  FuelCapacity: 55,
+  IndentNumber: 'XX0000YY',
+  DriverName: 'Viacheslav',
+  setDriverName: function setDriverName() {
+    var name = document.getElementById('name_one').value;
 
-      for (var key in this) {
-        if (this[key] === name && key === 'DriverName') {
-          alert('This name is already assigned to a driver. Enter another name.');
-          return;
-        }
-      }
-
-      if (!isNaN(name) || name === '') {
-        alert('Enter a valid name');
-        return;
-      } else {
-        this.DriverName += ',' + name;
-      }
-    },
-    showInfo: function showInfo() {
-      var rez = '<ul>';
-
-      for (var key in this) {
-        if (typeof this[key] !== 'function') {
-          rez += "<li><b>".concat(key, ":</b> ").concat(this[key], "</li>");
-        }
-      }
-
-      rez += '</ul>';
-      return rez;
-    },
-    calcFuelForDistance: function calcFuelForDistance() {
-      var distance = document.getElementById("number_one").valueAsNumber;
-      var fuel = distance * this.Using / 100;
-      var rechargeCnt = Math.trunc(fuel / this.FuelCapacity);
-      var middleSpeed = 100;
-      var normWay = 4;
-      var hoursOfDriving = distance / middleSpeed;
-      var totalPause = Math.trunc(hoursOfDriving / normWay);
-
-      for (var i = hoursOfDriving; i >= 4; i++) {
-        hoursOfDriving += totalPause;
-        break;
-      }
-
-      this.Fuel = fuel;
-      this.RechargeCnt = rechargeCnt;
-      this.HoursOfDriving = hoursOfDriving;
-      return {
-        Fuel: fuel,
-        RechargeCnt: rechargeCnt,
-        HoursOfDriving: hoursOfDriving
-      };
+    if (this.DriverName.includes(name)) {
+      alert('This name is already assigned to a driver. Enter another name.');
+      return nameInfo;
     }
-  };
-  var nameInfo = car.setDriverName(); // I don't know how show information about (new name and fuel) use different button. I can user only first button 
 
-  document.getElementById('car_table').innerHTML = car.showInfo(), nameInfo;
+    if (!isNaN(name) || name === '') {
+      alert('Enter a valid name');
+      return nameInfo;
+    } else {
+      this.DriverName += ',' + name;
+      return car.showInfo();
+    }
+  },
+  showInfo: function showInfo() {
+    var rez = '<ul>';
+
+    for (var key in this) {
+      if (typeof this[key] !== 'function') {
+        rez += "<li class=\"list-group-item\"><b>".concat(key, ":</b> ").concat(this[key], "</li>");
+      }
+    }
+
+    rez += '</ul>';
+    return rez;
+  },
+  calcFuelForDistance: function calcFuelForDistance() {
+    var distance = document.getElementById("number_one").valueAsNumber;
+    var fuel = (distance * this.Using / 100).toFixed(1);
+    var rechargeCnt = Math.trunc(fuel / this.FuelCapacity);
+    var middleSpeed = 100;
+    var normWay = 4;
+    var hoursOfDriving = distance / middleSpeed;
+    var totalPause = Math.trunc(hoursOfDriving / normWay);
+
+    for (var i = hoursOfDriving; i >= 4; i++) {
+      hoursOfDriving += totalPause;
+      break;
+    }
+
+    return {
+      Fuel: fuel,
+      RechargeCnt: rechargeCnt,
+      HoursOfDriving: hoursOfDriving
+    };
+  }
+};
+
+function My_car() {
+  document.getElementById('car_table').innerHTML = car.showInfo();
+}
+
+function newService() {
   var fuelInfo = car.calcFuelForDistance();
   document.getElementById('fuel_table').innerHTML = "<p><b>Fuel:</b> ".concat(fuelInfo.Fuel, "</p><p><b>Recharge Count:</b> ").concat(fuelInfo.RechargeCnt, "</p><p><b>HoursOfDriving:</b> ").concat(fuelInfo.HoursOfDriving, "</p>");
-} // function addName(car,name) {
-//     const nameInfo = car.setDriverName();
-//     if(confirm(`Do you want to add ${name}?`)){
-//         document.getElementById('car_table').innerHTML = nameInfo;
-//     }
-// }
+}
 
+function newName() {
+  var nameInfo = car.setDriverName();
+
+  if (confirm("Do you want to add new name?")) {
+    document.getElementById('car_table').innerHTML = nameInfo;
+  }
+}
 
 1.; // Створити об'єкт, що описує час (години, хвилини, секунди), і такі функції для роботи з цим об'єктом:
 // 1)Для виведення часу на екран.
@@ -92,65 +92,64 @@ function My_task1() {
 // 4)Зміни часу на передану кількість годин.
 // 5)Враховуйте, що в останніх 3-х функціях, при зміні однієї частини часу, може змінитися і інша. Наприклад: якщо до часу «20:59:45» додати 30 секунд, то повинно вийти «21:00:15», а не «20:59:75». Також потрібно передбачити можливість того що користувач може передати 150 секунд, або 75 хвилин.
 
-function myTime() {
-  var addZero = function addZero(n) {
-    return n < 10 ? '0' + n : '' + n;
-  };
-
-  var time = {
-    h: document.getElementById("figures_one").valueAsNumber,
-    m: document.getElementById("figures_two").valueAsNumber,
-    s: document.getElementById("figures_three").valueAsNumber,
-    time2sec: function time2sec() {
-      return this.h * 3600 + this.m * 60 + this.s; // функція,що переводить час в секунди
-    },
-    sec2time: function sec2time(sec) {
-      var hours = Math.floor(sec / 3600);
-      var minutes = Math.floor(sec / 60) - hours * 60;
-      var seconds = sec % 60;
-      return {
-        h: hours,
-        m: minutes,
-        s: seconds
-      };
-    },
-    // Внутрішня функція для нормалізації часу (перетворення зайвих секунд та хвилин)
-    norm2time: function norm2time() {
-      if (this.m === 60) {
-        this.h += this.m / 60;
-      }
-
-      if (this.h >= 24) {
-        this.h % 24;
-      }
-
-      if (this.s >= 60) {
-        this.m += Math.floor(this.s / 60);
-        this.s %= 60;
-      }
-
-      if (this.m >= 60) {
-        this.h += Math.floor(this.m / 60);
-        this.m %= 60;
-      }
-    },
-    addSec: function addSec() {
-      var plusSec = document.getElementById("name_sec").valueAsNumber;
-
-      if (plusSec >= 60) {
-        this.m += Math.floor(plusSec / 60);
-        this.s += plusSec %= 60;
-      } else {
-        this.s += plusSec;
-      }
-
-      return showTime; // I have problem with with function to add new seconds
+var time = {
+  h: document.getElementById("figures_one").valueAsNumber,
+  m: document.getElementById("figures_two").valueAsNumber,
+  s: document.getElementById("figures_three").valueAsNumber,
+  time2sec: function time2sec() {
+    return this.h * 3600 + this.m * 60 + this.s; // функція,що переводить час в секунди
+  },
+  sec2time: function sec2time(sec) {
+    var hours = Math.floor(sec / 3600);
+    var minutes = Math.floor(sec / 60) - hours * 60;
+    var seconds = sec % 60;
+    return {
+      h: hours,
+      m: minutes,
+      s: seconds
+    };
+  },
+  // Внутрішня функція для нормалізації часу (перетворення зайвих секунд та хвилин)
+  norm2time: function norm2time() {
+    if (this.m === 60) {
+      this.h += this.m / 60;
     }
-  };
+
+    if (this.h >= 24) {
+      this.h %= 24;
+    }
+
+    if (this.s >= 60) {
+      this.m += Math.floor(this.s / 60);
+      this.s %= 60;
+    }
+
+    if (this.m >= 60) {
+      this.h += Math.floor(this.m / 60);
+      this.m %= 60;
+    }
+  }
+};
+
+var addZero = function addZero(n) {
+  return n < 10 ? '0' + n : '' + n;
+};
+
+function myTime() {
   var sec = time.time2sec();
   time.norm2time();
   var showTime = time.sec2time(sec);
-  var newSec = time.addSec();
   document.getElementById('time-result').innerHTML = "".concat(addZero(showTime.h), ":").concat(addZero(showTime.m), ":").concat(addZero(showTime.s));
-  document.getElementById('sec-result').innerHTML = "".concat(addZero(newSec.h), ":").concat(addZero(newSec.m), ":").concat(addZero(newSec.s));
-}
+} // addSec: function () {
+//     let plusSec = document.getElementById(`name_sec`).valueAsNumber;
+//     if(plusSec>= 60) {
+//         this.m += Math.floor(plusSec / 60);
+//         this.s += (plusSec %= 60);
+//     }else {
+//         this.s += plusSec;
+//     }
+//     return showTime;
+//     // I have problem with with function to add new seconds
+// }
+// const newSec = time.addSec();
+// document.getElementById('sec-result').innerHTML = `${addZero(newSec.h)}:${addZero(newSec.m)}:${addZero(newSec.s)}`;
--- 05-js-object/assets/JS/main.js
+++ 05-js-object/assets/JS/main.js
@@ -4,93 +4,82 @@
 //3)Перевірка водія на наявність його ім’я у списку
 //4)Підрахунок необхідного часу та кількості палива для подолання переданої відстані з середньою швидкістю. Враховуй, що через кожні 4 години дороги водієві необхідно робити перерву на 1 годину. 
 
-function My_task1() {
+const car = {
+    Brand: 'Skoda',
+    Model: 'Oktavia',
+    'Year of productin': 2009,
+    Color: 'DarkGrey',
+    'Max speed': 320,
+    Using: 6.5,
+    IsNew: false,
+    IsMechanic: true,
+    FuelCapacity: 55,
+    IndentNumber: 'XX0000YY',
+    DriverName: 'Viacheslav', 
 
-    const car = {
-        Brand: 'Skoda',
-        Model: 'Oktavia',
-        'Year of productin': 2009,
-        Color: 'DarkGrey',
-        'Max speed': 320,
-        Using: 6.5,
-        IsNew: false,
-        IsMechanic: true,
-        FuelCapacity: 55,
-        IndentNumber: 'XX0000YY',
-        DriverName: 'Viacheslav',
-
-        setDriverName: function() {
-            const name = document.getElementById(`name_one`).value;
-
-            for (let key in this) {
-                if (this[key] === name && key === 'DriverName') {
-                    alert('This name is already assigned to a driver. Enter another name.');
-                    return;
-                }
-            }
-            if (!isNaN(name) || name === '') {
-                alert('Enter a valid name');
-                return;
-            }else {
-                this.DriverName +=',' + name;
-            }
-            
-        },  
-
-        showInfo: function () {
-            let rez = '<ul>';
-            for (let key in this) {
-                if (typeof (this[key]) !== 'function') {
-                    rez += `<li><b>${key}:</b> ${this[key]}</li>`;
-                }
-            }
-            rez += '</ul>';
-            return rez;
-        },
-        calcFuelForDistance: function() {
-           const distance = document.getElementById(`number_one`).valueAsNumber;
-           const fuel = distance * this.Using / 100;
-           const rechargeCnt = Math.trunc(fuel / this.FuelCapacity);
-           const middleSpeed = 100;
-           const normWay = 4;
-           let hoursOfDriving = distance / middleSpeed;
-           let totalPause = Math.trunc(hoursOfDriving / normWay);
-           for (let i = hoursOfDriving; i>= 4; i++){
-                    hoursOfDriving += totalPause;
-                    break
-                }
-           this.Fuel = fuel;
-           this.RechargeCnt = rechargeCnt;
-           this.HoursOfDriving = hoursOfDriving;
-           return {
-            Fuel:fuel,
-            RechargeCnt:rechargeCnt,
-            HoursOfDriving:hoursOfDriving
-           }
+    setDriverName: function() {
+        const name = document.getElementById('name_one').value;
+    
+        if (this.DriverName.includes(name)) {
+            alert('This name is already assigned to a driver. Enter another name.');
+            return nameInfo;
         }
-        
+        if (!isNaN(name) || name === '') {
+            alert('Enter a valid name');
+            return nameInfo;
+        } else {
+            this.DriverName += ',' + name;
+            return car.showInfo();
+        }
+    },
+
+    showInfo: function () {
+        let rez = '<ul>';
+        for (let key in this) {
+            if (typeof (this[key]) !== 'function') {
+                rez += `<li class="list-group-item"><b>${key}:</b> ${this[key]}</li>`;
+            }
+        }
+        rez += '</ul>';
+        return rez;
+    },
+    calcFuelForDistance: function() {
+       const distance = document.getElementById(`number_one`).valueAsNumber;
+       const fuel = (distance * this.Using / 100).toFixed(1);
+       const rechargeCnt = Math.trunc(fuel / this.FuelCapacity);
+       const middleSpeed = 100;
+       const normWay = 4;
+       let hoursOfDriving = distance / middleSpeed;
+       let totalPause = Math.trunc(hoursOfDriving / normWay);
+       for (let i = hoursOfDriving; i>= 4; i++){
+                hoursOfDriving += totalPause;
+                break
+            }
+       return {
+        Fuel:fuel,
+        RechargeCnt:rechargeCnt,
+        HoursOfDriving:hoursOfDriving
+       }
     }
-
-    const nameInfo = car.setDriverName();
-    // I don't know how show information about (new name and fuel) use different button. I can user only first button 
-    document.getElementById('car_table').innerHTML = car.showInfo(),nameInfo;
-
-    const fuelInfo = car.calcFuelForDistance();
-    document.getElementById('fuel_table').innerHTML = `<p><b>Fuel:</b> ${fuelInfo.Fuel}</p><p><b>Recharge Count:</b> ${fuelInfo.RechargeCnt}</p><p><b>HoursOfDriving:</b> ${fuelInfo.HoursOfDriving}</p>`;
-
+    
 }
 
 
-// function addName(car,name) {
-//     const nameInfo = car.setDriverName();
-//     if(confirm(`Do you want to add ${name}?`)){
-//         document.getElementById('car_table').innerHTML = nameInfo;
-//     }
-// }
+function My_car() {
+    document.getElementById('car_table').innerHTML = car.showInfo(); 
+}
 
+function newService() {
+    const fuelInfo = car.calcFuelForDistance();
+    document.getElementById('fuel_table').innerHTML = `<p><b>Fuel:</b> ${fuelInfo.Fuel}</p><p><b>Recharge Count:</b> ${fuelInfo.RechargeCnt}</p><p><b>HoursOfDriving:</b> ${fuelInfo.HoursOfDriving}</p>`;
+}
 
-
-
+function newName() {
+    const nameInfo = car.setDriverName();
+    if(confirm(`Do you want to add new name?`)){
+        document.getElementById('car_table').innerHTML = nameInfo;
+    }
+}
 
 
 1.// Створити об'єкт, що описує час (години, хвилини, секунди), і такі функції для роботи з цим об'єктом:
@@ -100,71 +89,76 @@ function My_task1() {
 // 4)Зміни часу на передану кількість годин.
 // 5)Враховуйте, що в останніх 3-х функціях, при зміні однієї частини часу, може змінитися і інша. Наприклад: якщо до часу «20:59:45» додати 30 секунд, то повинно вийти «21:00:15», а не «20:59:75». Також потрібно передбачити можливість того що користувач може передати 150 секунд, або 75 хвилин.
 
-function myTime() {
-    const addZero = n => n<10?'0'+n:''+n;
+const time = {
 
-    const time = {
+    h:document.getElementById(`figures_one`).valueAsNumber,
+    m:document.getElementById(`figures_two`).valueAsNumber,
+    s:document.getElementById(`figures_three`).valueAsNumber,
 
-        h:document.getElementById(`figures_one`).valueAsNumber,
-        m:document.getElementById(`figures_two`).valueAsNumber,
-        s:document.getElementById(`figures_three`).valueAsNumber,
+    time2sec:function() {
+        return this.h * 3600 + this.m * 60 + this.s; 
+        // функція,що переводить час в секунди
+    },
+    sec2time: function (sec) {
+        let hours = Math.floor(sec / 3600);
+        let minutes = Math.floor(sec / 60) - (hours * 60);
+        let seconds = sec % 60;
 
-        time2sec:function() {
-            return this.h * 3600 + this.m * 60 + this.s; 
-            // функція,що переводить час в секунди
-        },
-        sec2time: function (sec) {
-            let hours = Math.floor(sec / 3600);
-            let minutes = Math.floor(sec / 60) - (hours * 60);
-            let seconds = sec % 60;
+        return {
+            h: hours,
+            m: minutes,
+            s: seconds
+        };
+    },
 
-            return {
-                h: hours,
-                m: minutes,
-                s: seconds
-            };
-        },
-        // Внутрішня функція для нормалізації часу (перетворення зайвих секунд та хвилин)
-        norm2time: function () {
-            if (this.m === 60) {
-                this.h += this.m / 60;
-            }
-            if(this.h >= 24) {
-                this.h % 24;
-            }
-            if (this.s >= 60) {
-                this.m += Math.floor(this.s / 60);
-                this.s %= 60;
-            }
-
-            if (this.m >= 60) {
-                this.h += Math.floor(this.m / 60);
-                this.m %= 60;
-            }
-        
-        },
-        addSec: function () {
-            let plusSec = document.getElementById(`name_sec`).valueAsNumber;
-
-            if(plusSec>= 60) {
-                this.m += Math.floor(plusSec / 60);
-                this.s += (plusSec %= 60);
-            }else {
-                this.s += plusSec;
-            }
-            return showTime;
-            // I have problem with with function to add new seconds
+    // Внутрішня функція для нормалізації часу (перетворення зайвих секунд та хвилин)
+    norm2time: function () {
+        if (this.m === 60) {
+            this.h += this.m / 60;
+        }
+        if(this.h >= 24) {
+            this.h %= 24;
+        }
+        if (this.s >= 60) {
+            this.m += Math.floor(this.s / 60);
+            this.s %= 60;
         }
 
+        if (this.m >= 60) {
+            this.h += Math.floor(this.m / 60);
+            this.m %= 60;
+        }
     }
+    
+};
+
+const addZero = n => n<10?'0'+n:''+n;
+
+function myTime() {
 
     const sec = time.time2sec();
     time.norm2time();
     const showTime = time.sec2time(sec);
-    const newSec = time.addSec();
 
     document.getElementById('time-result').innerHTML = `${addZero(showTime.h)}:${addZero(showTime.m)}:${addZero(showTime.s)}`;
-    document.getElementById('sec-result').innerHTML = `${addZero(newSec.h)}:${addZero(newSec.m)}:${addZero(newSec.s)}`;
-
 }
+
     
+
+// addSec: function () {
+        //     let plusSec = document.getElementById(`name_sec`).valueAsNumber;
+
+        //     if(plusSec>= 60) {
+        //         this.m += Math.floor(plusSec / 60);
+        //         this.s += (plusSec %= 60);
+        //     }else {
+        //         this.s += plusSec;
+        //     }
+        //     return showTime;
+        //     // I have problem with with function to add new seconds
+        // }
+
+
+            // const newSec = time.addSec();
+
+            // document.getElementById('sec-result').innerHTML = `${addZero(newSec.h)}:${addZero(newSec.m)}:${addZero(newSec.s)}`;
